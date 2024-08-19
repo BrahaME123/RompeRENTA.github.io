0faa3c0db13a37f8945aa51d69908e1238067e14
BrahaME123 <gamermaxjuegos@gmail.com>
2024-08-19 01:34:05 -0600

    Update Wpp.js

--- JS/Wpp.js
+++ JS/Wpp.js
@@ -1,21 +1,11 @@
-document.querySelectorAll('.whatsapp-button').forEach(button => {
-    button.addEventListener('click', function() {
-      // Encuentra el contenedor padre del botón y saca su id
-      let articuloId = this.parentNode.id;
-      
-      // Define el número de WhatsApp al que quieres enviar el mensaje
-      let numeroWhatsApp = "+528134179056"; // Número en formato internacional sin el "+"
-      
-      // Crea el mensaje que quieres enviar
-      let mensaje = `Hola, estoy interesado en rentar el artículo con ID: ${articuloId}. ¿Podrías darme más información?`;
-      
-      // Crea el enlace de WhatsApp
-      let enlace = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(mensaje)}`;
-      
-      // Abre el enlace de WhatsApp en una nueva pestaña
-      window.open(enlace, '_blank');
-      
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.whatsapp-button').forEach(button => {
+        button.addEventListener('click', function() {
+            let articuloId = this.parentNode.id;
+            let numeroWhatsApp = "528134179056"; // Número en formato internacional sin el "+"
+            let mensaje = `Hola, estoy interesado en rentar el artículo con ID: ${articuloId}. ¿Podrías darme más información?`;
+            let enlace = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(mensaje)}`;
+            window.open(enlace, '_blank');
+        });
     });
-
-  });
-  
+});
