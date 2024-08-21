708f5cbcf16642e7e4c8833b756b83ef67f251f6
BrahaME123 <gamermaxjuegos@gmail.com>
2024-08-21 16:28:38 -0600

    INfocontact

--- JS/WppContact2.js
+++ JS/WppContact2.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('.whatsapp-btn2').forEach(button => {
         button.addEventListener('click', function() {
-            let numeroWhatsApp = "528117389778"; // Número en formato internacional sin el "+"
+            let numeroWhatsApp = "528115685758"; // Número en formato internacional sin el "+"
             let mensaje = `Hola, ¿Podrías darme más información acerca de las rompedoras??`;
             let enlace = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(mensaje)}`;
             window.open(enlace, '_blank');
