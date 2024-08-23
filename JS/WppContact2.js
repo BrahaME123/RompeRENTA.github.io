08683d5710e722145f63564e0c245432b91227c1
BrahaME123 <gamermaxjuegos@gmail.com>
2024-08-22 23:10:06 -0600

    Update WppContact2.js

--- JS/WppContact2.js
+++ JS/WppContact2.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('.whatsapp-btn2').forEach(button => {
         button.addEventListener('click', function() {
+            let articuloId = this.parentNode.id;
             let numeroWhatsApp = "528115685758"; // Número en formato internacional sin el "+"
-            let mensaje = `Hola, ¿Podrías darme más información acerca de las rompedoras??`;
+            let mensaje = `Hola, estoy interesado en rentar el artículo con ID: ${articuloId}. ¿Podrías darme más información?`;
             let enlace = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(mensaje)}`;
             window.open(enlace, '_blank');
         });
